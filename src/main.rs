392721f653d4ea7c9c7035854d44a257ce4ddd52
Your Name <you@example.com>
2021-07-25 23:42:35 +0800

    字符串
    Signed-off-by: Your Name <you@example.com>

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,71 @@
-use std::vec;
-
-// 1 创建空的 vector: Vec<T>
-// 2 创建包含初始值的vector
-// 3 丢弃vector
-// 4 读取元素
-// 5 更新
+// 1 创建一个空String
+// 2 通过字面值创建一个String
+// 2 1、使用String::from()
+// 2 2、使用str的方式
+// 3 更新String
+// 3 1、push_str
+// 3 2、push
+// 3 3、使用 "+" 合并字符串
+// 3 4、 使用format
+// 4 使用String 索引
+// 5  str 索引
 // 6 遍历
-// 7 使用枚举
-
+// 6 1、 chars
+// 6 2、 bytes
 fn main() {
-    // 1 创建空的的vector
-    // let mut  v: Vec<i32> = Vec::new();
-    let mut v: Vec<i32> = Vec::new();
-    //  v.push(1);
+    // 创建空的字符串
+    let mut s0 = String::new(); // mut可以修改字符串
+    s0.push_str("hello");
+    println!("s0 = {}", s0);
 
-    // 2 创建包含初始值的vector
-    let v = vec![1, 2, 3];
+    // 2 通过字面值创建一个String
+    let s1 = String::from("init some thing");
+    println!("{}", s1);
 
-    //  3 丢弃
-    {
-        //作用域
-        let v1 = vec![1, 2, 3];
-    }
+    let s1 = "init some thing".to_string();
+    println!("{}", s1);
 
-    // 4 读取元素
 
-    let one: &i32 = &v[0];
-    // let four : &i32 = &v[3];
-    println!("one = {}", one);
-    println!("one = {}", *one);
+    let mut s2 = String::from("hello");
+    s2.push_str(", world");
+    let ss = "  ！".to_string();
+    s2.push_str(&ss); //引用方法
+    println!("{}",s2);
+    println!("ss = {}",ss);
 
-    // 推荐使用第二种方法
-    match v.get(1) {
-        Some(value) => println!("two = {}", value),
-        _ => println!("None do nting"),
-    }
+    let mut s2 = String::from("tes");
+    s2.push('m'); // 只能添加一个字符 需要使用单引号
+    // s2.push('mx'); // error
+    // s2.push("m");   // error
+    println!("{}",s2);
 
-    //  更新操作
-    let mut v2: Vec<i32> = Vec::new();
-    v2.push(1);
-    v2.push(2);
-    v2.push(3);
-    // match v2.get(2) {
-    //     Some(value) => println!("v2 = {}", value),
-    //     _ => println!("None do nting"),
-    // }
-    //6  遍历
-    //  （1） 不可变遍历
-    for i in &v2 {
-        println!("i  = {}", i);
-    }
-    // （2） 可变的遍历
 
-    for i in &mut v2 {
-        *i += 1;
-        println!("i += {}", i);
-    }
+    let s1 = "hello".to_string();
+    let s2 =String::from(", world");
+    let s3 = s1 + &s2;
+    println!("s3 = {}",s3);
+    // println!("s1 = {}",s1);  // error s1的所有权 全部赋值给了s3
+    println!("s2 = {}",s2);
 
-    //  7
-    enum Context {
-        Text(String),
-        Float(f32),
-        Int(i32),
-    };
-    let c = vec![
-        Context::Text(String::from("string")),
-        Context::Int(-1),
-        Context::Float(0.001),
-    ];
+    let s341 = String::from("tic");
+    let s342 = String::from("tac");
+    let s343 = String::from("toe");
+    let s344 = format!("{}-{}-{}",s341,s342,s343); //format！ 和 println! 类似 一样的
+    println!("s344 = {}",s344);
+    println!("s341 = {}",s341);
+    println!("s342 = {}",s342);
+    println!("s343 = {}",s343);
 
-    // 8 补充
+    let s4 = String::from("hello");
+    // let s41 = s4[2]; // 不能被索引
+    println!("s4 = {}",s4.len());
 
-    let mut v = vec![1, 2, 3, 4, 5];
-    let first = &v[0];
-    v.push(6);
-    println!("first = {}", first); //不可变的值
+    let s4 = String::from("你好");
+    println!("s4 = {}",s4.len());
+
+    let hello = "你好";
+    let h5 = &hello[0..3];
+    println!("h5 = {}",h5);
 
     println!("hello world！");
 }
