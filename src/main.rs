95e1d4b89961b9c4eecd3ebb018ab1b659c81a42
access <access@gmail.com>
2021-08-02 19:41:41 +0800

    模块1

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,9 @@ mod factory {
             println!("produce refrigerator!");
         }
     }
-
-    mod produce_washing_machine {
+    pub mod produce_washing_machine {
         // 洗衣机
-        fn produce_washing_maching() {
+        pub fn produce_washing_maching() {
             println!("produce washing machine !");
         }
     }
@@ -16,8 +15,8 @@ mod factory {
 
 fn main() {
     // 调用模块使用
-  factory::produce_refrigerator::produce_re();
-
+    factory::produce_refrigerator::produce_re();
+    factory::produce_washing_machine::produce_washing_maching();
     println!("hello world !")
 }
 
