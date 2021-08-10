cfca143ed5a32742bf5dc785e039d07501bd14c2
Your Name <you@example.com>
2021-08-10 17:39:16 +0800

    panice

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
-
 //1、rust语言将错误分为两个类别：可恢复错误和不可恢复错误
 //（1）可恢复错误通常代表向用户报告错误和重试操作是合理的情况，例如未找到文件。rust中使用Result<T,E>来实现。
 //（2）不可恢复错误是bug的同义词，如尝试访问超过数组结尾的位置。rust中通过panic！来实现。
 // 2 panic!
-
-// 3  使用BAXKTRACE=1
+// 3  使用BAXKTRACE=1 错误处理
 // 4 Result<T,E>
+// enum Result<T， E>{
+// ok(T)
+// Err(E)
+// }
 // 5 简写
 
+use  std::fs::File;
 fn main() {
+    // let f = File::open("hello.txt");
+    // let r = match f {
+    //     Ok(file) => file,
+    //     Err(error) => panic!("error: {}", error),
+    // };
 
-    panic!("crash here");
+    // 简写
+    // let f = File::open("hello.txt").unwrap();
+    let f =  File::open("hello.txt").expect("FIle to open hello.txt");
+
+    // panic!("crash here");
 }
