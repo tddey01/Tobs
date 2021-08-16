7cc28284695d5a746c0dbb8aa951527df063514b
access <access@gmail.com>
2021-08-16 17:02:03 +0800

    测试用例

---
+++ mylib/src/animal.rs
@@ -0,0 +1,18 @@
+pub mod dog {
+    pub fn hello() {
+        println!("wangwang");
+    }
+
+    pub fn is_dog() -> bool{
+        true
+    }
+}
+
+pub mod cat {
+    pub fn hello() {
+        println!("miaomaio");
+    }
+    pub fn is_cat() -> bool {
+        true
+    }
+}
--- mylib/src/lib.rs
+++ mylib/src/lib.rs
@@ -1,10 +1,27 @@
-pub mod factory;  // 模块声明
+pub mod animal;
 
 #[cfg(test)]
 mod tests {
+    // use animal::Dog;
+    // use animal::Cat;
+    use crate::animal::*;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
+
+    #[test]
+    fn use_cat() {
+        // cat::hello()
+        assert_eq!(true,  cat::is_cat());
+    }
+
+    #[test]
+    fn user_dog(){
+        // assert_eq!(true,animal::Dog::is_dog());
+        assert_eq!(true,dog::is_dog());
+    }
+
 
 }
