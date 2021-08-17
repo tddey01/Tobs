51b13a4117108b3feb251331bf1c9ff08a10c498
access <access@gmail.com>
2021-08-17 16:49:34 +0800

    delete debug

--- mylib/target/debug/deps/mylib-4c77b4f7c6ef01b2.d
+++
@@ -1,6 +0,0 @@
-C:\Users\access\go\src\Tobs\mylib\target\debug\deps\mylib-4c77b4f7c6ef01b2.exe: src\lib.rs src\animal.rs
-
-C:\Users\access\go\src\Tobs\mylib\target\debug\deps\mylib-4c77b4f7c6ef01b2.d: src\lib.rs src\animal.rs
-
-src\lib.rs:
-src\animal.rs:
